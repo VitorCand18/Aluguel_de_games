9ec6191ac792968b1207176d7978640e74d7ec38
Vitor Candido <vitor.candido900@gmail.com>
2025-02-17 13:41:18 -0300

    Aviso de devolucao

--- js/app.js
+++ js/app.js
@@ -4,12 +4,27 @@ function alterarStatus(numero) {
     let botao = documentoSelecionado.querySelector('.dashboard__item__button') // Selecionar a classe incluida no botão > <a>
 
     if (imagem.classList.contains("dashboard__item__img--rented")) {
-        imagem.classList.remove("dashboard__item__img--rented");
-        botao.classList.remove("dashboard__item__button--return");
-        botao.textContent = "Alugar";
+        devolverJogo(numero); // Passando o número do jogo para a função
     } else {
         imagem.classList.add("dashboard__item__img--rented");
         botao.classList.add("dashboard__item__button--return");
         botao.textContent = "Devolver";
+    }
+}
+
+function devolverJogo(numero){
+    let documentoSelecionado = document.getElementById("game-" + numero); // Pegando o ID das 3 opções no HTML
+    let imagem = documentoSelecionado.querySelector('.dashboard__item__img'); // Pegando a imagem
+    let botao = documentoSelecionado.querySelector('.dashboard__item__button'); // Pegando o botão
+    let nome = documentoSelecionado.querySelector('.dashboard__item__name'); // Pegando o nome do jogo
+
+    let resposta = confirm(`Deseja devolver o jogo ${nome.textContent}?`); // Corrigi a forma de pegar o nome (adicionando `.textContent`)
+
+    if (resposta) {
+        imagem.classList.remove("dashboard__item__img--rented");
+        botao.classList.remove("dashboard__item__button--return");
+        botao.textContent = "Alugar";
+    } else {
+        alterarStatus(numero); // Passando o número
     }
 }
