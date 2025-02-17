75b8f13cde04347f0398d220c7a12966ae89aa19
Vitor Candido <vitor.candido900@gmail.com>
2025-02-17 12:24:59 -0300

    Inclusao da logica no JS

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 function alterarStatus(numero) {
     let documentoSelecionado = document.getElementById("game-" + numero); // Pegando o ID das 3 opções no HTML
-    let imagem = documentoSelecionado.querySelector('.dashboard__item__img'); // incluir um ('.') na frente para informar que é uma classe > DIV
-    let botao = documentoSelecionado.querySelector('.dashboard__item__button') // Selecionar a classe incluida no botão > Botão    
-    let nomeJogo = documentoSelecionado.querySelector('.dashboard__item__name') // Pegando a tag P
+    let imagem = documentoSelecionado.querySelector('.dashboard__item__img'); // incluir um ('.') na frente para informar que é uma classe > <div>
+    let botao = documentoSelecionado.querySelector('.dashboard__item__button') // Selecionar a classe incluida no botão > <a>
 
-    alert(nomeJogo.textContent);
+    if (imagem.classList.contains("dashboard__item__img--rented")) {
+        imagem.classList.remove("dashboard__item__img--rented");
+        botao.classList.remove("dashboard__item__button--return");
+        botao.textContent = "Alugar";
+    } else {
+        imagem.classList.add("dashboard__item__img--rented");
+        botao.classList.add("dashboard__item__button--return");
+        botao.textContent = "Devolver";
+    }
 }
