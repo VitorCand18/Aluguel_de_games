a2b3b0fbb96144d23c97d78e86ffe536f8978984
Vitor Candido <vitor.candido900@gmail.com>
2025-02-17 14:58:21 -0300

    Inseri um contador no console

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let jogosAlugados = 0 // Para armazenar a quantidade de jogos alugados
+
 function alterarStatus(numero) {
     let documentoSelecionado = document.getElementById("game-" + numero); // Pegando o ID das 3 opções no HTML
     let imagem = documentoSelecionado.querySelector('.dashboard__item__img'); // incluir um ('.') na frente para informar que é uma classe > <div>
@@ -9,6 +11,9 @@ function alterarStatus(numero) {
         imagem.classList.add("dashboard__item__img--rented");
         botao.classList.add("dashboard__item__button--return");
         botao.textContent = "Devolver";
+
+        jogosAlugados++; // acrescenta no console
+        contaJogosAlugados() // imprime no console
     }
 }
 
@@ -24,7 +29,14 @@ function devolverJogo(numero){
         imagem.classList.remove("dashboard__item__img--rented");
         botao.classList.remove("dashboard__item__button--return");
         botao.textContent = "Alugar";
+
+        jogosAlugados--;// acrescenta no console
+        contaJogosAlugados() // imprime no console
     } else {
         alterarStatus(numero); // Passando o número
     }
+}
+
+function contaJogosAlugados(){
+    console.log(`Quantidade de jogos alugados: ${jogosAlugados}`)
 }
